de92b201526cc0de89b58e000f864e2e97a51905
HumaGitGud <khomidov.huma@student.greenriver.edu>
2024-10-05 01:44:54 -0700

    added read method with rng for names and ages

--- Java/src/App.java
+++ Java/src/App.java
@@ -1,7 +1,19 @@
+import java.util.Random;
+
 public class App {
     public static void main(String[] args) {
-        String[] names = {"Joe", "Bob", "Zoe"};
-        String[] lastNames = {"McBoat", "McFly", "McMillan"};
-        int[] ages = {20, 22, 21};
+        String[] names = {"Joe", "Akira", "Amelie", "Thomas", "Ivar"};
+        String[] lastNames = {"McFly", "Dupont", "McMillan", "Ragnarson", "Yamada"};
+        int[] ages = {28, 22, 26, 18, 32};
+
+        read(names, lastNames, ages);
+    }
+
+    public static void read(String[] names, String[] lastNames, int[] ages) {
+        Random rand = new Random();
+        int gen1 = rand.nextInt(5);
+        int gen2 = rand.nextInt(5);
+        int gen3 = rand.nextInt(5);
+        System.out.println(names[gen1] + " " + lastNames[gen2] + " " + ages[gen3]);
     }
 }
