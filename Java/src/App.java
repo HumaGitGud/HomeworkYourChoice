73a5c847f14296c8c8fef679a1eb9f057b82e374
HumaGitGud <khomidov.huma@student.greenriver.edu>
2024-10-05 01:22:59 -0700

    Created a java project with some basic code

---
+++ Java/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) {
+        String[] names = {"Joe", "Bob", "Zoe"};
+        String[] lastNames = {"McBoat", "McFly", "McMillan"};
+        int[] ages = {20, 22, 21};
+    }
+}
